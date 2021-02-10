11401ee2adb639c55d22055d078293600c7f5bf7
jleatham78 <janeleleatham01@gmail.com>
2021-02-09 22:20:23 -0700

    added hashed passwords

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 class User extends Model {}
 
@@ -33,7 +34,16 @@ User.init(
         }
     },
     {
-        //TABLE CONFIG OPTIONS GO HERE
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+              },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
