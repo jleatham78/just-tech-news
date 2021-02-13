bee1523003f1f32e3a5bc43b945747ec17ae1bb2
jleatham78 <janeleleatham01@gmail.com>
2021-02-12 21:21:30 -0700

    added comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 router.get('/', (req, res) => {
     Post.findAll({
+      order: [['created_at', 'DESC']],
       attributes: [
           'id', 
           'post_url', 
@@ -11,13 +12,20 @@ router.get('/', (req, res) => {
           'created_at',
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
-      order: [['created_at', 'DESC']],
-      include: [
+        include: [
         {
-          model: User,
-          attributes: ['username']
-        }
-      ]
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
     })
       .then(dbPostData => res.json(dbPostData))
       .catch(err => {
@@ -39,6 +47,14 @@ router.get('/', (req, res) => {
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
           ],
           include: [
+              {
+                  model: Comment,
+                  attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                  include: {
+                    model: User,
+                    attributes: ['username']
+                  }
+              },
               {
                   model: User,
                   attributes: ['username']
