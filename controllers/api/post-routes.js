5a877bfa822dba81bce5156dffc6df5a047a10f4
jleatham78 <janeleleatham01@gmail.com>
2021-02-14 21:29:41 -0700

    lesson 1

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,30 +4,33 @@ const sequelize = require('../../config/connection');
 
 router.get('/', (req, res) => {
     Post.findAll({
-      order: [['created_at', 'DESC']],
       attributes: [
-          'id', 
-          'post_url', 
-          'title', 
-          'created_at',
+        'id',
+        'post_url',
+        'title',
+        'created_at',
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        include: [
+      ],
+      include: [
         {
-            model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-              model: User,
-              attributes: ['username']
-            }
-          },
-          {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
             model: User,
             attributes: ['username']
           }
-        ]
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
     })
-      .then(dbPostData => res.json(dbPostData))
+      .then(dbPostData => {
+          const posts = dbPostData.map(post => post.get({ plain: true }));
+        // pass a single post object into the homepage template
+        res.render('homepage', { posts });
+      })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
