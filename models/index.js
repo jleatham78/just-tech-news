73496a4ee5a5eae293f99c55c4ddc797401cbacc
jleatham78 <janeleleatham01@gmail.com>
2021-02-20 14:34:49 -0700

    bugs

--- models/index.js
+++ models/index.js
@@ -6,12 +6,14 @@ const Comment = require('./Comment');
 // create associations
 User.hasMany(Post, {
   foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  onDelete: 'SET NULL',
+  allowNull: false
 });
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  onDelete: 'SET NULL',
+  allowNull: false
 });
 
 User.belongsToMany(Post, {
@@ -19,54 +21,64 @@ User.belongsToMany(Post, {
   as: 'voted_posts',
 
   foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  onDelete: 'SET NULL',
+  allowNull: false
 });
 
 Post.belongsToMany(User, {
   through: Vote,
   as: 'voted_posts',
   foreignKey: 'post_id',
-  onDelete: 'SET NULL'
+  onDelete: 'SET NULL',
+  allowNull: false
 });
 
 Vote.belongsTo(User, {
   foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  onDelete: 'SET NULL',
+  allowNull: false
 });
 
 Vote.belongsTo(Post, {
   foreignKey: 'post_id',
-  onDelete: 'SET NULL'
+  onDelete: 'SET NULL',
+  allowNull: false
 });
 
 User.hasMany(Vote, {
   foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  onDelete: 'SET NULL',
+  allowNull: false
 });
 
 Post.hasMany(Vote, {
   foreignKey: 'post_id',
-  onDelete: 'SET NULL'
+  onDelete: 'SET NULL',
+  allowNull: false
 });
 
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  onDelete: 'SET NULL',
+  allowNull: false
 });
 
 Comment.belongsTo(Post, {
   foreignKey: 'post_id',
-  onDelete: 'SET NULL'
+  onDelete: 'SET NULL',
+  allowNull: false
 });
 
 User.hasMany(Comment, {
   foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  onDelete: 'SET NULL',
+  allowNull: false
 });
 
 Post.hasMany(Comment, {
   foreignKey: 'post_id',
-  onDelete: 'SET NULL'
+  onDelete: 'SET NULL',
+  allowNull: false
 });
 
 module.exports = { User, Post, Vote, Comment };
